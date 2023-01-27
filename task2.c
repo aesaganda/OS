a82417c9009853fb0c8587fad97d93a585532bab
aesaganda <erensaganda@marun.edu.tr>
2023-01-28 01:03:33 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   .vscode/settings.json
    	modified:   task2.c

--- task2.c
+++ task2.c
@@ -1,1 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void yaprak()
+{
+    char dosya_isim[20];
+    sprintf(dosya_isim, "%d.txt", getpid());
+
+    FILE *fp = fopen(dosya_isim, "w");
+    srand(time(getpid()));
+    fprintf(fp, "%d", (rand() % 100));
+    fclose(fp);
+}
+
+int parent(int pid1, int pid2)
+{
+    char dosya_isim1[20];
+    sprintf(dosya_isim1, "%d.txt", pid1);
+
+    char dosya_isim2[20];
+    sprintf(dosya_isim2, "%d.txt", pid2);
+
+    char dosya_isim3[20];
+    sprintf(dosya_isim3, "%d.txt", getpid());
+
+    FILE *fp1, *fp2, *fp3;
+    fp1 = fopen(dosya_isim1, "r");
+    fp2 = fopen(dosya_isim2, "r");
+
+    int sol, sag;
+    fscanf(fp1, "%d", &sol);
+    fscanf(fp2, "%d", &sag);
+    fclose(fp1);
+    fclose(fp2);
+
+    fp3 = fopen(dosya_isim3, "w");
+    fprintf(fp3, "%d", sol + sag);
+    fclose(fp3);
+}
+
+int agac_olustur(int yukseklik)
+{
+    if (yukseklik == 0)
+    {
+        yaprak();
+        return;
+    }
+
+    pid_t pid1 = fork();
+
+    if (pid1 > 0)
+    {
+        int pid2 = fork();
+
+        if (pid2 > 0)
+        {
+            // parent işlem
+        }
+
+        else if (pid2 == 0)
+        {
+            // left child
+            agac_olustur(--yukseklik);
+        }
+    }
+
+    else if (pid1 == 0)
+    {
+        // right child
+        agac_olustur(--yukseklik);
+    }
+}
